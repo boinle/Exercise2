8380e73d96464c0dec50780d3b3e4f6b8c8db6e9
boinle <lenhatthaonguyen2006@gmail.com>
2026-01-23 13:24:49 +0700

    2301

---
+++ Exercise2301/Exercise5_RemoveAnElement.java
@@ -0,0 +1,18 @@
+package Exercise2301;
+
+import java.util.*;
+
+public class Exercise5_RemoveAnElement {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Nhap so thu " + (i + 1) + ": ");
+            int x = sc.nextInt();
+            numbers.add(x);
+        }
+        numbers.remove(3);
+        System.out.println(numbers);
+    }
+}
---
+++ Exercise2301/Exercise6_CountOddAnEvenNumbers.java
@@ -0,0 +1,26 @@
+package Exercise2301;
+
+import java.util.*;
+
+public class Exercise6_CountOddAnEvenNumbers {
+
+    public static void main(String[] args) {
+        Random rd = new Random();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            int x = rd.nextInt(50) + 1;
+            System.out.println("So thu " + (i + 1) + " la: " + x);
+            numbers.add(x);
+        }
+        int demChan = 0;
+        int demLe = 0;
+        for (int i = 0; i < 15; i++) {
+            if (numbers.get(i) % 2 == 0) {
+                demChan++;
+            } else {
+                demLe++;
+            }
+        }
+        System.out.println("Co " + demChan + " so chan va " + demLe + " so le");
+    }
+}
